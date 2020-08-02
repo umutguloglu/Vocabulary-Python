9c72b10e61fde4ace91445ebc60094fe878129cd
Umut Guloglu <umutguloglu71@gmail.com>
2020-08-02 10:49:04 +0300

    Basic 'Number of Words' error handling messages has been added by using If statements.

--- Dictionary.py
+++ Dictionary.py
@@ -6,19 +6,8 @@ from random import randint
 _Green="\33[32m"  #_Green="\33[0m"
 _Yellow="\33[33m" #_Yellow="\33[0m"
 _Cyan="\33[36m"   #_Cyan="\33[0m"
-
-def _random():
-    with open("sample.json", "r") as f:
-        dictionary= json.load(f)
-        number=randint(1,_length(dictionary)) - 1 # Array starts with 0.
-        word=list(dictionary["Words"])[number]
-        print("\33[32m Word : ", end="")
-        print(word)
-        print("\33[33m Def. : ", end="")
-        print(dictionary["Words"][word]["Definition"])
-        print("\33[36m Example : ", end="")
-        print(dictionary["Words"][word]["Examples"])
-        print("\33[0m Reading has been done succesfully. \33[0m")
+_NoColor="\33[0m"
+#------------------------------------------------------------------------------
 
 def _length(_dict):
     if _dict["Number of Words"] == len(_dict["Words"]):
@@ -26,6 +15,30 @@ def _length(_dict):
     else:
         print("There is a mistake in calculating length.")
         return -1
+
+def _random():
+    with open("sample.json", "r") as f:
+        dictionary= json.load(f)
+        __length=_length(dictionary)
+        if __length==0 :
+            print("There is not any word in the vocabulary yet.")
+            return -1
+        elif __length<0:
+            print("Length is negative.")
+            return -1
+        else :
+            number=randint(1,__length) - 1 # Array starts with 0.
+            word=list(dictionary["Words"])[number]
+            print(_Green,"Word : ", end="")
+            print(word)
+            print(_Yellow,"Def. : ", end="")
+            print(dictionary["Words"][word]["Definition"])
+            print(_Cyan,"Example : ", end="")
+            print(dictionary["Words"][word]["Examples"])
+            _preRepeat=dictionary["Words"][word]["Number of Repeats"]
+            dictionary["Words"][word]["Number of Repeats"]=_preRepeat+1
+            with open("sample.json", "w") as f:
+                json.dump(dictionary,f,indent=4)
 
 _type=input("\t\tWelcome! What do you want?\n" +
 "1-) To add word, press 'a' and Enter. \n" +
@@ -38,6 +51,7 @@ if _type=="a" :
     pass
 elif _type=="r" :
     _random()
+    print(_NoColor,"Reading has been done succesfully.")
     pass
 elif _type=="s" :
     pass
